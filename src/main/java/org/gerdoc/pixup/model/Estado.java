799cb5e343e76ed6177f46b2612e9ddf00ffbecc
gerdocEmmax <germangutierrez@emmax.com.mx>
2025-04-11 04:52:54 +0100

    Primera versión hibernate

--- src/main/java/org/gerdoc/pixup/model/Estado.java
+++ src/main/java/org/gerdoc/pixup/model/Estado.java
@@ -1,29 +1,18 @@
 package org.gerdoc.pixup.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity( name = "TBL_ESTADO")
 public class Estado extends Catalogo
 {
+    @Column( name ="estado" , nullable = false )
     private String nombre;
-
-    public Estado()
-    {
-    }
-
-    public String getNombre()
-    {
-        return nombre;
-    }
-
-    public void setNombre(String nombre)
-    {
-        this.nombre = nombre;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "Estado{" +
-                "nombre='" + nombre + '\'' +
-                ", id=" + id +
-                '}';
-    }
 }
